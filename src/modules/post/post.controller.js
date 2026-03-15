a15e2a5a131befc8bc633e9a2a6a26fbf1ea17a3
VinhHV28 <vinhhv28@gmail.com>
2026-03-16 02:54:19 +0700

    feat: implement partner post routes and enhance post creation logic with image upload support

--- src/modules/post/post.controller.js
+++ src/modules/post/post.controller.js
@@ -1,5 +1,5 @@
 import { uploadToCloudinary } from "../../middlewares/uploadMiddleware.js";
-import { errorResponse, successResponse } from "../../utils/responseHelper";
+import { errorResponse, successResponse } from "../../utils/responseHelper.js";
 import postModel from "./post.model.js";
 
 export const getPartnerPosts = async (req, res) => {
@@ -15,69 +15,75 @@ export const getPartnerPosts = async (req, res) => {
   }
 };
 
-export const createPostByPartner = async (req, res) => {
+export const partnerCreatePost = async (req, res) => {
   try {
-    const {
-      destinationId,
-      title,
-      content,
-      bannerTitle,
-      bannerAlt,
-      galleryAlts,
-    } = req.body;
+    const { destinationId, title, content, bannerTitle, bannerAlt } = req.body;
 
-    if (!destinationId)
+    if (!destinationId) {
       return errorResponse(res, "Địa điểm cho bài viết này là bắt buộc", 400);
+    }
 
     if (!title || !content) {
       return errorResponse(res, "Tiêu đề và nội dung không được để trống", 400);
     }
 
-    let bannerData = { title: bannerTitle, alt: bannerAlt, image: "" };
+    let bannerData = {
+      title: bannerTitle || "",
+      alt: bannerAlt || "",
+      image: "",
+    };
 
     if (req.files && req.files["bannerImage"]) {
       const result = await uploadToCloudinary(
         req.files["bannerImage"][0].buffer,
-        "vivuviet/banners",
+        "vivuviet/partners/banners",
       );
-
       bannerData.image = result.secure_url;
     }
 
     let galleryData = [];
-    if (res.files && res.files["galleryImages"]) {
+    if (req.files && req.files["galleryImages"]) {
       const uploadPromises = req.files["galleryImages"].map((file, index) => {
         const altKey = `galleryAlt_${index}`;
         const altText = req.body[altKey] || "";
-
-        return uploadToCloudinary(file.buffer, "vivuviet/gallery").then(
-          (res) => ({
-            image: res.secure_url,
-            alt: altText,
-          }),
-        );
+        return uploadToCloudinary(
+          file.buffer,
+          "vivuviet/partners/gallery",
+        ).then((res) => ({
+          image: res.secure_url,
+          alt: altText,
+        }));
       });
+
       galleryData = await Promise.all(uploadPromises);
     }
 
-    const createdBy = req.user._id;
-    const postType = "partner";
     const newPost = await postModel.create({
       destinationId,
       title,
       content,
       banner: bannerData,
       gallery: galleryData,
-      postType,
-      relatedArticles: [],
-      postedDate: postedDate || new Date(),
-      createdBy,
+      postType: "partner",
+      createdBy: req.user._id,
       status: "pending",
+      postedDate: new Date(),
+      relatedArticles: [],
     });
 
-    return successResponse(res, newPost, "Tạo bài viết thành công", 201);
+    return successResponse(
+      res,
+      newPost,
+      "Gửi bài viết thành công. Vui lòng chờ quản trị viên phê duyệt",
+      201,
+    );
   } catch (error) {
-    console.error(error);
-    return errorResponse(res, "Lỗi khi tạo bài đăng", 500, error);
+    console.error("Partner Create Post Error:", error);
+    return errorResponse(
+      res,
+      "Lỗi hệ thống khi đối tác tạo bài đăng",
+      500,
+      error.message,
+    );
   }
 };
